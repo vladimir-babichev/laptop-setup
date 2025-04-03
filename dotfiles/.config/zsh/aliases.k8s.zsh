1290be070e3126f5cfdcab9714e756e064a8fd2d
Vladimir Babichev <vladimir.babichev@dh.com>
2025-04-03 16:27:22 +0100

    Added aliases to check kube-api connectivity from within the pod

--- dotfiles/.config/zsh/aliases.k8s.zsh
+++ dotfiles/.config/zsh/aliases.k8s.zsh
@@ -99,6 +99,9 @@ alias krund='k run --dry-run=client -o yaml'
 alias kshell='k shell'
 alias kgansr='k api-resources --verbs=list --namespaced -o name | xargs -n 1 kubectl get --show-kind --ignore-not-found'
 alias kgsdates='getTLSSecretDetails'
+alias kcpapiconnw='kcpapiconnw_f() { kx "$@" -- sh -c "wget -S -O- https://\${KUBERNETES_SERVICE_HOST}/api --no-check-certificate --header=\"Authorization: Bearer \$(cat /run/secrets/kubernetes.io/serviceaccount/token)\""; }; kcpapiconnw_f'
+alias kcpapiconnc='kcpapiconnc_f() { kx "$@" -- sh -c "curl -v https://\${KUBERNETES_SERVICE_HOST}/api --insecure -H \"Authorization: Bearer \$(cat /run/secrets/kubernetes.io/serviceaccount/token)\""; }; kcpapiconnc_f'
+alias kcpapiconn='kcpapiconnw'
 
 alias cleanyaml="yq e '"'del(.. | select(tag == "!!map") | (.status, .creationTimestamp, .generation, .selfLink, .uid, .resourceVersion, .managedFields, ."kubectl.kubernetes.io/last-applied-configuration"))'"' -"
 alias cleanjson="jq '"'del(.. | select(. == "" or . == null)) | walk(if type == "object" then del(.status, .creationTimestamp, .generation, .selfLink, .uid, .resourceVersion, .managedFields, ."kubectl.kubernetes.io/last-applied-configuration") else . end) | del(.. | select(. == {}))'"'"
